e8369fce11e235b8795f82adc72d16d62a1f78bf
kaizzz-452 <kzkoay.26@ichat.sp.edu.sg>
2026-07-31 14:49:42 +0800

    Add files via upload

---
+++ LED/LED.ino
@@ -0,0 +1,19 @@
+#define LED_GREEN 5
+#define LED_RED 4
+#define LED_BLUE 6
+#define LED_YELLOW 7
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_RED,OUTPUT);   
+  pinMode(LED_GREEN,OUTPUT); 
+  pinMode(LED_BLUE,OUTPUT); 
+  pinMode(LED_YELLOW,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
