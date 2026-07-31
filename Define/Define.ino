fd3bf6fe3ab524a262cb600d8ffa1943b27941a6
kaizzz-452 <kzkoay.26@ichat.sp.edu.sg>
2026-07-31 15:00:41 +0800

    Add files via upload

---
+++ Define/Define.ino
@@ -0,0 +1,23 @@
+#define LED_GREEN 5
+#define LED_RED 4
+#define LED_BLUE 6
+#define LED_YELLOW 7
+#define BUTTONK1 8
+#define BUTTONK2 9
+#define KNOB_PIN A0
+#define LDR_PIN A2
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_RED,OUTPUT);   
+  pinMode(LED_GREEN,OUTPUT); 
+  pinMode(LED_BLUE,OUTPUT); 
+  pinMode(LED_YELLOW,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
