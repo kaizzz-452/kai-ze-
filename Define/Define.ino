d52db44728b02c9139610e0dc50df5cc23086f99
kaizzz-452 <kzkoay.26@ichat.sp.edu.sg>
2026-08-12 14:21:57 +0800

    Define and pinMode
    changed the define and pinMode

--- Define/Define.ino
+++ Define/Define.ino
@@ -1,25 +1,39 @@
-#define LED_GREEN 5
+// Pin Definitions
 #define LED_RED 4
+#define LED_GREEN 5
 #define LED_BLUE 6
 #define LED_YELLOW 7
-#define BUTTONK1 8
-#define BUTTONK2 9
+#define BUTTON_K1 8   // Blue Button
+#define BUTTON_K2 9   // Yellow Button
 #define KNOB_PIN A0
 #define LDR_PIN A2
+#define BUZZER_PIN 3
+#define DISPLAY_CLK 10
+#define DISPLAY_DIO 11
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_RED,OUTPUT);   
-  pinMode(LED_GREEN,OUTPUT); 
-  pinMode(LED_BLUE,OUTPUT); 
-  pinMode(LED_YELLOW,OUTPUT);
-  pinMode(BUTTON_K1, INPUT_PULLUP);
-  pinMode(BUTTON_K2, INPUT_PULLUP);
   Serial.begin(9600);
+
+  pinMode(LED_RED, OUTPUT);   
+  pinMode(LED_GREEN, OUTPUT); 
+  pinMode(LED_BLUE, OUTPUT); 
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(DISPLAY_CLK, OUTPUT);
+  pinMode(DISPLAY_DIO, OUTPUT);
   
+  pinMode(BUTTON_K1, INPUT_PULLUP); // Blue
+  pinMode(BUTTON_K2, INPUT_PULLUP); // Yellow
+  pinMode(KNOB_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  
 }
