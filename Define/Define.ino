f12f61401c53735620dfb1e606c94c3f278970bd
JIWUNN <changjiwunn@gmail.com>
2026-08-13 21:28:16 +0800

    Implement reminder and alert functions
    Added reminderfrequency and overduealert functions for handling reminders and alerts.

--- Define/Define.ino
+++ Define/Define.ino
@@ -12,7 +12,9 @@
 #define DISPLAY_DIO 11
 
 
-
+//Functions
+int reminderfrequency(int knobValue); //Ji Wunn
+void overduealert(); //Ji Wunn
 
 void setup() {
   Serial.begin(9600);
@@ -52,4 +54,43 @@ const int victoryMelody[5] = {523, 659, 784, 880, 1047};
 
 void loop() {
   
+}
+
+int reminderfrequency(int knobValue) //Function, Ji Wunn
+{
+    int remindertime;
+    if (knobValue <= 400) //knob value less than or equal 400?
+    {
+        remindertime = 20000;
+    }
+    else
+        if (knobValue > 800) //knob value greater than 800?
+        {
+            remindertime = 60000;
+        }
+        else
+        {
+            remindertime = 40000;
+        }
+    return remindertime; 
+} 
+
+void overduealert() //Function, Ji Wunn
+{
+    digitalWrite(LED_RED, HIGH);
+
+    do  {
+    tone(BUZZER_PIN, 800, 500); //Digital Buzzer
+    tone(BUZZER_PIN, 600, 500);
+    tone(BUZZER_PIN, 400, 500);
+    } while (digitalRead(BUTTON_K1) == 1);
+
+    digitalWrite(LED_RED, LOW);
+
+    delay(300);
+    while(digitalRead(BUTTON_K1) == 0);
+
+    digitalWrite(LED_GREEN, HIGH);
+    delay(2000);
+    digitalWrite(LED_GREEN, LOW);
 }
